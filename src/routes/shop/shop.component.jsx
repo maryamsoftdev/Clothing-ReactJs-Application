d7232f650ade09d15e5dd0cf7d5ade4bfc2c4328
maryamsoftdev <nabeelahmaryaam@gmail.com>
2022-12-27 00:09:01 +0500

    created product display grid

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,17 +3,19 @@ import { useContext } from "react";
 
 
 import { ProductsContext } from "../../contexts/products.context";
+
+import ProductCard from '../../components/product-card/product-card.components';
+
+import './shop.styles.scss';
+
 const Shop = () => {
 
   const{products} = useContext(ProductsContext)
 
     return(
-        <div>
-            {products.map(({id, name}) => (
-          <div key={id}>
-            <h1>{name}</h1>
-          </div>
-
+        <div className="products-container">
+            {products.map((product) => (
+          <ProductCard key={product.id} product={product}/>
             ))}
         </div>
     );
